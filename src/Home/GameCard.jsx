2969b1c251c4fbc2ae9e9c8ffff7576814e3360b
Adithya_G <adithyaacharya710@gmail.com>
2025-03-26 17:54:25 +0530

    altered home page

--- src/Home/GameCard.jsx
+++ src/Home/GameCard.jsx
@@ -1,24 +1,24 @@
 import * as React from 'react';
 import { Button } from '@mui/material';
-import "./GameCard.css";
+import gc from "./GameCard.module.css";
 
 export default function GameCard({gameName, gameDescription, img, background, handleClick, scrollLeft, scrollRight}){
     return(
-        <div className="GameContainer">
-            <div className="gameContainer" style={{backgroundColor: background, width: "`100vw"}}>
-                <div className="game">
-                    <div className="gameDtl">
+        <div className={gc.GameContainer}>
+            <div className={gc.gameContainer} style={{backgroundColor: background, width: "`100vw"}}>
+                <div className={gc.game}>
+                    <div className={gc.gameDtl}>
                         <h1>{gameName}</h1>
                         <p>{gameDescription}</p>
                     </div>
-                    <div className="image">
+                    <div className={gc.image}>
                         <img src={img} alt="" />
                     </div>
                 </div>   
-                <div className='btns'>
-                    {scrollLeft && <Button variant='outlined' color="light" onClick={scrollLeft} className='btn'>prev</Button>} 
-                    <Button variant='contained' color='light' size='large' onClick={() => handleClick(gameName)} className='play'>Play</Button>
-                    {scrollRight && <Button variant='outlined' color="light" onClick={scrollRight} className='btn'>next</Button>}       
+                <div className={gc.btns}>
+                    {scrollLeft && <Button variant='contained' size='large' onClick={scrollLeft} className={gc.btnPrev}>prev</Button>} 
+                    <Button variant='contained' size='large' onClick={() => handleClick(gameName)} className={gc.btnPlay}>Play</Button>
+                    {scrollRight && <Button variant='contained' size='large' onClick={scrollRight} className={gc.btnNext}>next</Button>}       
                 </div>
             </div>
         </div>
