3505a8838f4375b7dcd9af651da49b66cc2113cd
Adithya_G <adithyaacharya710@gmail.com>
2025-03-26 17:36:17 +0530

    updated the ui of StonePaperScissor game

--- src/StonePaperScissor/Sps.jsx
+++ src/StonePaperScissor/Sps.jsx
@@ -1,20 +1,20 @@
-import "./Sps.css";
-
 import stoneImg from "./assets/stone-img.png";
 import paperImg from "./assets/paper-img.png";
 import scissorImg from "./assets/sciccors-img.png";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
+import sps from "./Sps.module.css";
 
 const gameChoices = ["stone", "paper", "scissor"];
 let gameInfo = "Choose any one on the left to Start the Game";
-let userChoice = "";
-let gameChoice = "";
 const goal = 5;
 
 export default function StonePaperScissor(){
-    let [start, setStart] = useState( true);
+    let [start, setStart] = useState(true);
+    let [isActive, setIsActive] = useState(true);
     let [userScore, setUserScore] = useState(0);
     let [gameScore, setGameScore] = useState(0);
+    let [userChoice, setUserChoice] = useState(0);
+    let [gameChoice, setGameChoice] = useState(0);
     let [middleText, setMiddleText] = useState("Lets Begin!");
 
     useEffect(() =>{
@@ -24,28 +24,32 @@ export default function StonePaperScissor(){
         }else if(gameScore == goal){
             gameInfo = "You Lost!";
             setStart(!start);
-        }else if(userScore != goal && userScore > gameScore){
-            gameInfo = "You are winning";
-        }else if(gameScore != goal && gameScore > userScore){
-            gameInfo = "Trust Your Luck!";
+        }else {
+            gameInfo = "";
         }
     }, [gameScore, userScore]);
 
     function makeChoice(event){
-        if(start === true){
-            userChoice = event.target.id;
+        if(start === true) {
+            if(!isActive) {
+                return;
+            }
+            setIsActive(false);
+            let userChoice = event.target.id;
             let random = Math.floor(Math.random() * 3);
-            gameChoice = gameChoices[random];
-            compare();
-        }else{
-            setStart(!start);
+            let gameChoice = gameChoices[random];
+            setUserChoice(userChoice);
+            setGameChoice(gameChoice);
+            compare(userChoice, gameChoice);
+        } else {
+            setStart(true);
+            setIsActive(true);
             setUserScore(0);
             setGameScore(0);
-            gameInfo = "Choose any one on the left to Start the Game";
         }
     }
 
-    function compare(){
+    function compare(userChoice, gameChoice){
         switch(userChoice){
             case "stone": {
                 if(gameChoice == "paper"){
@@ -57,7 +61,7 @@ export default function StonePaperScissor(){
                 }else{
                     setMiddleText("tie!");
                 }
-                return;
+                break;
             };
             case "paper": {
                 if(gameChoice == "stone"){
@@ -69,7 +73,7 @@ export default function StonePaperScissor(){
                 }else{
                     setMiddleText("tie!");
                 }
-                return;
+                break;
             };
             case "scissor": {
                 if(gameChoice == "paper"){
@@ -81,32 +85,45 @@ export default function StonePaperScissor(){
                 }else{
                     setMiddleText("tie!");
                 }
-                return;
+                break;
             };
             default :{
                 setMiddleText("Invalid Choice");
             }
         }
+        setTimeout(() => setIsActive(true), 1000);
+        setTimeout(() => setUserChoice(""), 1000);
+        setTimeout(() => setGameChoice(""), 1000);
     }
 
     return(
-        <div className={userScore === goal ? "StonePaperScissor win": (gameScore === goal ? "StonePaperScissor loss": "StonePaperScissor")}>
-            <header>
-                <h1>STONE PAPER SEASOR</h1>
-                <h3>{gameInfo}</h3>
-                <div className="score"><h4 className="user-score">Your Score : {userScore}</h4><h4>The Goal is 5</h4><h4 className="game-score">Game Score : {gameScore}</h4></div>
-            </header>
-            <div className="container">
-                <div className="user">
-                    <div className={userChoice === "stone" ? "stone-u click": "stone-u"} ><img src={stoneImg} alt="Stone" id="stone" onClick={makeChoice} /></div>
-                    <div className={userChoice === "paper" ? "paper-u click": "paper-u"}><img src={paperImg} alt="Paper" id="paper" onClick={makeChoice} /></div>
-                    <div className={userChoice === "scissor" ? "scissor-u click": "scissor-u"}><img src={scissorImg} alt="Scissor" id="scissor" onClick={makeChoice} /></div>
+        <div className={userScore === goal ? `${sps.StonePaperScissor} ${sps.win}`: (gameScore === goal ? `${sps.StonePaperScissor} ${sps.loss}`: `${sps.StonePaperScissor}`)}>
+            <div className={sps.header}>
+                <div className={sps.left}>
+                    <h4 className={sps.currScoreTitle}>YOUR SCORE</h4>
+                    <h1 className={sps.currScore}>{userScore}</h1>
                 </div>
-                <h4 className="middle-text">{middleText}</h4>
-                <div className="game">
-                    <div className={gameChoice === "stone" ? "stone-g click": "stone-g"}><img src={stoneImg} alt="Stone" id="stone-g" /></div>
-                    <div className={gameChoice === "paper" ? "paper-g click": "paper-g"}><img src={paperImg} alt="Paper" id="paper-g"  /></div>
-                    <div className={gameChoice === "scissor" ? "scissor-g click": "scissor-g"}><img src={scissorImg} alt="Scissor" id="scissor-g" /></div>
+                <div className={sps.middle}>
+                    <h2 className={sps.name}>STONE PAPER SCISSOR</h2>
+                    <h5 className={sps.level}>Winning Score is - 5</h5>
+                    <p>{gameInfo}</p>
+                </div>
+                <div className={sps.right}>
+                    <h4 className={sps.highScoreTitle}>GAME SCORE</h4>
+                    <h1 className={sps.highScore}>{gameScore}</h1>
+                </div>
+            </div>
+            <div className={sps.container}>
+                <div className={sps.user}>
+                    <div className={userChoice === "stone" ? `${sps.stoneU} ${sps.click}`: `${sps.stoneU}`} ><img src={stoneImg} alt="Stone" id="stone" onClick={makeChoice} /></div>
+                    <div className={userChoice === "paper" ? `${sps.paperU} ${sps.click}`: `${sps.paperU}`}><img src={paperImg} alt="Paper" id="paper" onClick={makeChoice} /></div>
+                    <div className={userChoice === "scissor" ? `${sps.scissorU} ${sps.click}`: `${sps.scissorU}`}><img src={scissorImg} alt="Scissor" id="scissor" onClick={makeChoice} /></div>
+                </div>
+                <h4 className={sps.middleText}>{middleText}</h4>
+                <div className={sps.game}>
+                    <div className={gameChoice === "stone" ?  `${sps.stoneG} ${sps.click}`: `${sps.stoneG}`}><img src={stoneImg} alt="Stone" id="stone-g" /></div>
+                    <div className={gameChoice === "paper" ?  `${sps.paperG} ${sps.click}`: `${sps.paperG}`}><img src={paperImg} alt="Paper" id="paper-g"  /></div>
+                    <div className={gameChoice === "scissor" ? `${sps.scissorG} ${sps.click}`: `${sps.scissorG}`}><img src={scissorImg} alt="Scissor" id="scissor-g" /></div>
                 </div>
             </div>
         </div>
